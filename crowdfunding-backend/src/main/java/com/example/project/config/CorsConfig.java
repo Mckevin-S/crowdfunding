a67a4caab061d5775ace542392a99d61cafa36eb
Mckevin-S <kamguemmckevin@gmail.com>
2026-05-23 05:15:10 +0100

    parametre d'exposition en local

--- crowdfunding-backend/src/main/java/com/example/project/config/CorsConfig.java
+++ crowdfunding-backend/src/main/java/com/example/project/config/CorsConfig.java
@@ -16,11 +16,14 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         // Permettre à ces origines d'accéder à l'API (Frontend local, etc.)
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200", "http://localhost:5173"));
-        
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200",
+                "http://localhost:5173", "http://172.27.232.215:5173"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept",
+                "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        configuration
+                .setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
